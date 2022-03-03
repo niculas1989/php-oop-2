ebc978a00bb33b3b03d9366b7c00121c63b5d2a2
niculas1989 <nicolas.maranzano@libero.it>
2022-03-03 16:25:28 +0100

    TODO done - division between two files

---
+++ data/includes/SelectedProduct.php
@@ -0,0 +1,60 @@
+<?php
+require_once __DIR__ . '/Product.php';
+
+
+class SelectedProduct extends Product
+{
+    public $color;
+    public $price;
+    public $animal; //# (per che tipologia di animale serve)
+    public $type;
+
+    public function __construct($name, $brand, $amount, $color, $price, $animal, $type)
+    {
+        parent::__construct($name, $brand, $amount);
+        $this->setColor($color);
+        $this->setPrice($price);
+        $this->setAnimal($animal);
+        $this->setType($type);
+    }
+
+    public function setColor($color)
+    {
+        $this->color = $color;
+    }
+
+    public function getColor()
+    {
+        return $this->color;
+    }
+
+    public function setPrice($price)
+    {
+        $this->price = $price;
+    }
+
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    public function setAnimal($animal)
+    {
+        $this->animal = $animal;
+    }
+
+    public function getAnimal()
+    {
+        return $this->animal;
+    }
+
+    public function setType($type)
+    {
+        $this->type = $type;
+    }
+
+    public function getType()
+    {
+        return $this->type;
+    }
+}
