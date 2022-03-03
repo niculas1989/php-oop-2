f430d5cd7a108d13de1ee70a01dcbe5f389e0811
niculas1989 <nicolas.maranzano@libero.it>
2022-03-03 16:42:03 +0100

    Card.php done for now

--- data/includes/Card.php
+++ data/includes/Card.php
@@ -15,11 +15,41 @@ class Card extends Account
     private $code;
     private $expiration_date;
 
-    public function __construct($first_name, $last_name, $age, $card, $circuit, $code, $expiration_date)
+    public function __construct($first_name, $last_name, $age, $circuit, $code, $expiration_date)
     {
         parent::__construct($first_name, $last_name, $age);
+        $this->setCircuit($circuit);
+        $this->setCode($code);
+        $this->setExpirationDate($expiration_date);
+    }
+
+    public function setCircuit($circuit)
+    {
         $this->circuit = $circuit;
+    }
+
+    public function getCircuit()
+    {
+        return $this->circuit;
+    }
+
+    public function setCode($code)
+    {
         $this->code = $code;
+    }
+
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    public function setExpirationDate($expiration_date)
+    {
         $this->expiration_date = $expiration_date;
+    }
+
+    public function getExpirationDate()
+    {
+        return $this->expiration_date;
     }
 }
