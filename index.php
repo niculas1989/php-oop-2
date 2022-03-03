2bb17f89f2f67efb74cf7f4a2420366dc80061e1
niculas1989 <nicolas.maranzano@libero.it>
2022-03-03 14:15:36 +0100

    added comment

--- index.php
+++ index.php
@@ -8,6 +8,8 @@ I prodotti saranno oltre al cibo, anche giochi, cucce, etc.
 L'utente potrà sia comprare i prodotti senza registrarsi, oppure iscriversi e
 ricevere il 20% di sconto su tutti i prodotti.
 Il pagamento avviene con la carta di credito, che non deve essere scaduta.
+
+
 CONSIGLI DEL GIORNO
 Cerchiamo di fare un po' di analisi aiutandoci con qualche schemino (Sulla carta o su draw.io et similia)
 Occupiamoci di una classe alla volta, procedendo con le classi via via relazionate tra loro
@@ -15,3 +17,6 @@ Non preoccupatevi subito della visibilità: in un primo momento potete lasciare 
 Lasciamo perdere la validazione coi type hint: in questa fase ci sporcano il codice rendendo la lettura più difficile. Potrete aggiungerli una volta soddisfatti del lavoro finale!
 Non cercate di strafare, concentratevi sulle classi e le loro proprietà metodi, se riuscite a istanziare e far vedere i var_dump in pagina come fatto in classe va più che bene
 */
+
+/*
+//! provato ad utilizzare Draw-io, ma non mi viene comodo. Provo a schematizzare qui
