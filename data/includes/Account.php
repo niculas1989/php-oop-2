585a06b644a8b8d091ae77449d470fa9c98ab182
niculas1989 <nicolas.maranzano@libero.it>
2022-03-03 15:16:16 +0100

    added first steps for Account class

--- data/includes/Account.php
+++ data/includes/Account.php
@@ -10,3 +10,21 @@
     $logged (si aspetto un booleano come risposta)
 } 
 */
+
+class Account
+{
+    protected $first_name;
+    protected $last_name;
+    protected $age;
+    protected $card;
+    protected $logged;
+
+    public function __construct($first_name, $last_name, $age, $card, $logged)
+    {
+        $this->setFirstName($first_name);
+        $this->setLastName($last_name);
+        $this->setAge($age);
+        $this->setCard($card);
+        $this->setLogged($logged);
+    }
+}
