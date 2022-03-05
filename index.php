bae2fc0048948a0179b6a9f5a40412c5de908e22
niculas1989 <nicolas.maranzano@libero.it>
2022-03-05 15:46:19 +0100

    added comments

--- index.php
+++ index.php
@@ -4,12 +4,17 @@ require_once __DIR__ . '/data/includes/Account.php';
 require_once __DIR__ . '/data/includes/SelectedProduct.php';
 require_once __DIR__ . '/data/includes/Cart.php';
 
-
+//| provo a creare un nuovo account.
 $account = new Account('Nicolas', 'Maranzano', 32, 'Credit Card');
 var_dump($account);
 
+//| provo a creare una nuova carta.
 $my_card = new Card('Maestro', 123456789, 22, 1000);
 var_dump($my_card);
 
+//| setto l'indirizzo dell'account, con annessa "stampa" successiva.
 $account->setAddress('Italia', 'Palermo', 'Via degli arei', '90100');
 var_dump($account);
+
+
+$cart = new Cart();
